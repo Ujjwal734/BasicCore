73aa4f27aa119c968c5c738136a96d408ab0fc7f
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-01-30 02:09:59 +0530

    Quotient Remainder

---
+++ BasicCore/Division.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class Division
+    {
+        public void QutRem()
+        {
+            Console.WriteLine("Enter Number 1 and Number 2");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = num1 / num2;
+            Console.WriteLine("Quotient = "+ num3);
+            int num4 = num1 % num2;
+            Console.WriteLine("Remainde = "+ num4);
+          
+        }
+    }
+}
--- BasicCore/Program.cs
+++ BasicCore/Program.cs
@@ -1,7 +1,7 @@
 ﻿using BasicCore;
 Console.WriteLine("Hello, Welcome to Basic-Core Programe...!");
 Console.WriteLine("What you want to do...:- Choose Option ");
-Console.WriteLine("1. FlipCoin \n2. Leapyear \n3. Table of power 2\n4. Harmonic Number\n 5. Prime Factor\n 6. Exit");
+Console.WriteLine("1. FlipCoin \n2. Leapyear \n3. Table of power 2\n4. Harmonic Number\n5. Prime Factor\n6. Quotient Remainder\n7. Exit");
 int option = Convert.ToInt32(Console.ReadLine());
 bool flag= true;
 while (true)
@@ -33,6 +33,10 @@ while (true)
             factor.PrimFact(num);
             break;
         case 6:
+            Division division = new Division();
+            division.QutRem();
+            break;
+        case 7:
             flag = false;
             break;
         default:
