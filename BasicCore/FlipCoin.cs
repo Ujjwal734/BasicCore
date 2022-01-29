02a5a80d27f98e4e95bedcf9998b70247277f3c3
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-01-29 23:49:38 +0530

    CoinFlip Game

---
+++ BasicCore/FlipCoin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class FlipCoin
+    {
+        const double Check = 0.5;
+        double headcount = 0, tailcount = 0;
+        public void Flip()
+        {
+            Random random = new Random();
+            Console.WriteLine("Enter How many times you Flip the Coin :- ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < number; i++)
+            {
+                double option = random.NextDouble();
+                if (option < Check)
+                    headcount++;
+                else
+                    tailcount++;
+            }
+            Console.WriteLine("Head Count" +(headcount/number)*100);
+            Console.WriteLine("Tail Count" + (tailcount / number) * 100);
+        }
+    }
+}
--- BasicCore/Program.cs
+++ BasicCore/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, Welcome to Basic-Core Programe...!");
+﻿using BasicCore;
+Console.WriteLine("Hello, Welcome to Basic-Core Programe...!");
+Console.WriteLine("1. FlipCoin ");
+bool flag= true;
+while(true)
+{
+    int optionToExecute = Convert.ToInt32(Console.ReadLine());
+    switch (optionToExecute)
+    {
+        case 1:
+            FlipCoin flipcoin = new FlipCoin();
+            flipcoin.Flip();
+            break;
+        default:
+            Console.WriteLine("Choose correct option to Execute :-");
+            break;
+    }
+}
