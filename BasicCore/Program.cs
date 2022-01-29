d9dfa3d25772bdbd4e8824f49557d01c511cfef1
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-01-30 01:18:07 +0530

    Table of power 2

--- BasicCore/Program.cs
+++ BasicCore/Program.cs
@@ -1,8 +1,10 @@
 ﻿using BasicCore;
 Console.WriteLine("Hello, Welcome to Basic-Core Programe...!");
 Console.WriteLine("What you want to do...:- Choose Option ");
-Console.WriteLine("1. FlipCoin \n2. Leapyear \n3. Exit");
+Console.WriteLine("1. FlipCoin \n2. Leapyear \n3. Table of power 2\n4. Exit");
 int option = Convert.ToInt32(Console.ReadLine());
+bool flag= true;
+while(true)
 switch (option)
 {
     case 1:
@@ -14,6 +16,13 @@ switch (option)
             Console.WriteLine("Enter a year :- ");
             int yeer = Convert.ToInt32(Console.ReadLine());
             leapyear.Year(yeer);
+            break;
+    case 3:
+            Table table = new Table();
+            table.Power();
+            break;
+    case 4:
+            flag= false;
             break;
     default:
             Console.WriteLine("Choose correct option to Execute :-");
---
+++ BasicCore/Table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class Table
+    {
+        public void Power()
+        {
+            Console.WriteLine("Enter a power upto where you want a table :- ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine(2 + "^"+i +"=" +Math.Pow(2,i));
+            }
+        }
+    }
+}
