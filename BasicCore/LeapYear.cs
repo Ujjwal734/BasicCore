174194e701df97a7b5c4e107eee77ac9f983a869
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-01-30 00:56:54 +0530

    Leapyear

---
+++ BasicCore/LeapYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class LeapYeer
+    {
+        public void Year(int Yeer)
+        {
+            if ((Yeer > 999) && (Yeer < 10000))
+            {
+                if ((Yeer % 4 == 0) && (Yeer % 100 != 0) || (Yeer % 400 == 0))
+                    Console.WriteLine("{0} is a Leap year", +Yeer);
+                else
+                    Console.WriteLine("{0} is Not a Leap year", +Yeer);
+            }
+            else
+                Console.WriteLine("Enter Four digit Valid Year :- ");
+        }
+    }
+}
--- BasicCore/Program.cs
+++ BasicCore/Program.cs
@@ -1,18 +1,21 @@
 ﻿using BasicCore;
 Console.WriteLine("Hello, Welcome to Basic-Core Programe...!");
-Console.WriteLine("1. FlipCoin ");
-bool flag= true;
-while(true)
+Console.WriteLine("What you want to do...:- Choose Option ");
+Console.WriteLine("1. FlipCoin \n2. Leapyear \n3. Exit");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
 {
-    int optionToExecute = Convert.ToInt32(Console.ReadLine());
-    switch (optionToExecute)
-    {
-        case 1:
+    case 1:
             FlipCoin flipcoin = new FlipCoin();
             flipcoin.Flip();
             break;
-        default:
+    case 2:
+            LeapYeer leapyear = new LeapYeer();
+            Console.WriteLine("Enter a year :- ");
+            int yeer = Convert.ToInt32(Console.ReadLine());
+            leapyear.Year(yeer);
+            break;
+    default:
             Console.WriteLine("Choose correct option to Execute :-");
             break;
-    }
 }
