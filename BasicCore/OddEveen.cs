d3a7e70b4444d5c626ddbd5c945e12310d701257
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-01-30 23:43:03 +0530

    Check odd even

---
+++ BasicCore/OddEveen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class OddEveen
+    {
+        public void OddEven()
+        {
+            Console.WriteLine("Enter a number to check Number is Odd Or Even");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("Number is Even :- " + num);
+            else
+                Console.WriteLine("Number is Odd :- " + num);
+        }
+    }
+}
--- BasicCore/Program.cs
+++ BasicCore/Program.cs
@@ -1,7 +1,7 @@
 ﻿using BasicCore;
 Console.WriteLine("Hello, Welcome to Basic-Core Programe...!");
 Console.WriteLine("What you want to do...:- Choose Option ");
-Console.WriteLine("1. FlipCoin \n2. Leapyear \n3. Table of power 2\n4. Harmonic Number\n5. Prime Factor\n6. Quotient Remainder\n7.Swaping Number\n8. Exit");
+Console.WriteLine("1. FlipCoin \n2. Leapyear \n3. Table of power 2\n4. Harmonic Number\n5. Prime Factor\n6. Quotient Remainder\n7.Swaping Number\n8. Check odd Even\n9. Exit");
 int option = Convert.ToInt32(Console.ReadLine());
 bool flag= true;
 while (true)
@@ -42,6 +42,10 @@ while (true)
             swap.Swaping();
             break;
         case 8:
+            OddEveen oddEveen = new OddEveen();
+            oddEveen.OddEven();
+            break;
+        case 9:
             flag = false;
             break;
         default:
